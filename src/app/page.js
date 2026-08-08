1993b90002a510683ba196f1ea2d5fe4379de66a
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-08-08 18:23:28 +0600

    fix issues

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,10 @@
+import Image from "next/image";
+import UnderConstruction from "@/components/underConstruction/UnderConstruction";
+
 export default function Home() {
+  if (process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true") {
+    return <UnderConstruction></UnderConstruction>;
+  }
   return (
     <div className="text-2xl font-bold text-red-700">This is Home page</div>
   );
