6cebaa27fc98ea1e4ba81e1dc0fe30eca4de876d
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-08-08 18:09:38 +0600

    fix issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import UnderConstruction from "@/components/underConstruction/UnderConstruction";
-
 export default function Home() {
   return (
     <div className="text-2xl font-bold text-red-700">This is Home page</div>
