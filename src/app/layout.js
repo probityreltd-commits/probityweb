8a4e40ad6106635e235e3bbec6c932953549ba86
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-08-13 18:34:41 +0600

    add portfolio section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,15 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Manrope, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
 import UnderConstruction from "@/components/underConstruction/UnderConstruction";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const manrope = Manrope({
+  variable: "--font-manrope",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const playfair = Playfair_Display({
+  variable: "--font-playfair",
   subsets: ["latin"],
 });
 
@@ -25,9 +25,9 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${manrope.variable} ${playfair.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col font-sans antialiased">
         {maintenanceMode ? (
           <UnderConstruction />
         ) : (
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import Banner from "@/components/home/Banner";
+import Portfolio from "@/components/home/Portfolio";
 export default function Home() {
   return (
     <div className="">
       <Banner></Banner>
+      <Portfolio></Portfolio>
     </div>
   );
 }
